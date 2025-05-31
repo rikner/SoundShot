28bd136dffec2cdebcc8637cf1e51333bd2b71a2
Erik Werner <rikner@web.de>
2025-05-31 11:06:35 +0200

    Initial Commit

---
+++ SoundShot/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SoundShot
+//
+//  Created by Erik Werner on 31.05.25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
